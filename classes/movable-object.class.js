d8483996a57b80b9731bac2c26dc86d32aafee30
Stefan Redl <bernhardlaus@gmx.at>
2025-03-04 20:37:42 +0100

    Initial commit

---
+++ classes/movable-object.class.js
@@ -0,0 +1,21 @@
+class MovableObject {
+    positionX = 120;
+    positionY = 250;
+    img;
+    width = 100;
+    height = 150;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log("Moving right");
+    }
+
+    
+    moveLeft() {
+        
+    }
+}
